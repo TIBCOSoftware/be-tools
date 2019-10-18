9bb5a5ffd6f4ae3ffc22cb2227e3b47ae6d48604
dijadhav <dipakjadhav08@gmail.com>
2019-10-18 13:51:14 +0530

    For issue # 9 added below changes 1) BE And As compatible version in be_docker_instakk.pm. 2) Added code to find BE version from installer 2) Update shell script and docker files for run file

--- cloud/docker/s2i/create_builder_image.sh
+++ cloud/docker/s2i/create_builder_image.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+#Map used to store the BE and it's comapatible JRE version
+declare -A BE_VERSION_AND_JRE_MAP
+BE_VERSION_AND_JRE_MAP=( ["5.6.0"]="1.8.0" ["5.6.1"]="11")
+
 if [ -z "${USAGE}" ]; then
  USAGE="\nUsage: create_builder_image.sh"
 fi
@@ -18,12 +23,12 @@ S2I_DOCKER_FILE_BASE="bin/Dockerfile"
 S2I_DOCKER_FILE_APP="Dockerfile"
 ARG_DOCKERFILE_NAME="Dockerfile"
 ARG_EDITION="enterprise"
-ARG_VERSION="5.6.0"
+ARG_VERSION="5.6.1"
 ARG_ADDONS="na"
 ARG_INSTALLER_LOCATION="na"
 ARG_BE_HOTFIX="na"
 ARG_AS_HOTFIX="na"
-ARG_JRE_VERSION="1.8.0"
+ARG_JRE_VERSION="11"
 IS_S2I="true"
 ARG_APP_LOCATION="na"
 ARG_IMAGE_VERSION="na"
@@ -159,33 +164,35 @@ fi
 BLANK=""
 BE_PRODUCT="TIB_businessevents"
 INSTALLER_PLATFORM="_linux26gl25_x86_64.zip"
-BE_BASE_VERSION_REGEX="${BE_PRODUCT}-${ARG_EDITION}_${ARG_VERSION}${INSTALLER_PLATFORM}"
+
+BE_BASE_VERSION_REGEX="${BE_PRODUCT}-${ARG_EDITION}_*${INSTALLER_PLATFORM}"
 BE_HF_REGEX="${BE_PRODUCT}-${ARG_EDITION}_${ARG_VERSION}_HF"
 BE_PROCESS_ADDON_REGEX="${BE_PRODUCT}-process_${ARG_VERSION}${INSTALLER_PLATFORM}"
 BE_VIEWS_ADDON_REGEX="${BE_PRODUCT}-views_${ARG_VERSION}${INSTALLER_PLATFORM}"
+
 #AS_REGEX="TIB_activespaces_*_linux_x86_64.zip";
 #AS_HF_REGEX="TIB_activespaces*_HF-*_linux_x86_64.zip";
 
 
-#Check for 5.6.0 Installer  --------------------------------------
+#Check for BE Installer  --------------------------------------
 result=$(find $ARG_INSTALLER_LOCATION -name "$BE_BASE_VERSION_REGEX")
-len=$(find $ARG_INSTALLER_LOCATION -name "$BE_BASE_VERSION_REGEX" | wc -l)
+len=$(echo ${result} | wc -l)
 
 if [ $len -eq 0 ]; then
-	printf "\nERROR: TIBCO BusinessEvents (${ARG_VERSION}) is not present in the target directory. There should be only one.\n"
+	printf "\nERROR: TIBCO BusinessEvents Installer is not present in the target directory. There should be only one.\n"
 	exit 1;
 fi
 
-# Get all packages(base and hf) for 5.6.0 --------------------------------------
-bePckgs=$(find $ARG_INSTALLER_LOCATION -name "${BE_PRODUCT}-${ARG_EDITION}_${ARG_VERSION}*$INSTALLER_PLATFORM")
-bePckgsCnt=$(find $ARG_INSTALLER_LOCATION -name "${BE_PRODUCT}-${ARG_EDITION}_${ARG_VERSION}*$INSTALLER_PLATFORM" | wc -l)
+# Get all packages(base and hf)  --------------------------------------
+bePckgs=$(find $ARG_INSTALLER_LOCATION -name "${BE_PRODUCT}-${ARG_EDITION}_*$INSTALLER_PLATFORM")
+bePckgsCnt=$(find $ARG_INSTALLER_LOCATION -name "${BE_PRODUCT}-${ARG_EDITION}_*$INSTALLER_PLATFORM" | wc -l)
 
 
 #Get All HF for BE --------------------------------------
 beHfPckgs=$(find $ARG_INSTALLER_LOCATION -name "$BE_HF_REGEX*$INSTALLER_PLATFORM")
 beHfCnt=$(find $ARG_INSTALLER_LOCATION -name  "$BE_HF_REGEX*$INSTALLER_PLATFORM" | wc -l)
 
-# Check Single Base version for 5.6.0 exist, zero or one HF exist. --------------------------------------
+# Check Single Base version  exist, zero or one HF exist. --------------------------------------
 beBasePckgsCnt=$(expr ${bePckgsCnt} - ${beHfCnt})
 
 if [ $beBasePckgsCnt -gt 1 ]; then # If more than one base versions are present
@@ -198,9 +205,19 @@ elif [ $beBasePckgsCnt -le 0 ]; then # If HF is present but base version is not 
 	printf "\nERROR: TIBCO BusinessEvents HF is present but TIBCO BusinessEvents Base version is not present in the target directory.\n"
 	exit 1;
 elif [ $beBasePckgsCnt -eq 1 ]; then
+	#Find BE Version from installer
+	ARG_VERSION=$(echo "${bePckgs[0]##*/}" | sed -e "s/${INSTALLER_PLATFORM}/${BLANK}/g" |  sed -e "s/${BE_PRODUCT}-${ARG_EDITION}"_"/${BLANK}/g") 
+	#Find JER Version for given BE Version
+	ARG_JRE_VERSION=${BE_VERSION_AND_JRE_MAP[$ARG_VERSION]}
 	if [ $beHfCnt -eq 1 ]; then # If Only one HF is present then parse the HF version
-		beversion=$(echo "${beHfPckgs[0]}" | sed -e "s/${INSTALLER_PLATFORM}/${BLANK}/g")
-	    ARG_BE_HOTFIX=$(echo $beversion| cut -d'_' -f 5)
+		hfbeversion=$(echo "${beHfPckgs[0]}" | sed -e "s/${INSTALLER_PLATFORM}/${BLANK}/g")
+		if [ $ARG_VERSION == $hfbeversion];then
+			ARG_BE_HOTFIX=$(echo "${beversion}"| cut -d'_' -f 5)
+		else
+			printf "\nERROR: TIBCO BusinessEvents version in HF installer TIBCO BusinessEvents Base version is matching.\n"
+			exit 1;
+		fi 
+		
 	elif [ $beHfCnt -eq 0 ]; then
 		ARG_BE_HOTFIX="na"
 	fi
@@ -249,7 +266,7 @@ asHfPckgsCnt=$(find $ARG_INSTALLER_LOCATION -name "TIB_activespaces_*_HF-*_linux
 
 if [ $asPckgsCnt -gt 0 ]; then
 	asBasePckgsCnt=$(expr ${asPckgsCnt} - ${asHfPckgsCnt})
-
+	
 	if [ $asBasePckgsCnt -gt 1 ]; then # If more than one base versions are present
 		printf "\nERROR :More than one TIBCO Activespaces base versions are present in the target directory..\n"
 		exit 1;
