9db183a9514fe803303533ffce9175ee60312e7c
skbasit <38567736+skbasit@users.noreply.github.com>
2019-11-20 15:04:07 +0530

    Added license info to batch scripts.

--- cloud/docker/gvproviders/consul/setup.bat
+++ cloud/docker/gvproviders/consul/setup.bat
@@ -1,4 +1,6 @@
 @echo off
+@rem Copyright (c) 2019. TIBCO Software Inc.
+@rem This file is subject to the license terms contained in the license file that is distributed with this file.
 
 setlocal EnableExtensions EnableDelayedExpansion
 
