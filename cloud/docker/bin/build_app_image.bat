69e1857a32c9cb6e722118ffe516652fb95f1c9a
kagrawal-git <kagrawal@tibco.com>
2020-02-21 09:31:43 +0530

    Docker improvement phase1 changes

--- cloud/docker/bin/build_app_image.bat
+++ cloud/docker/bin/build_app_image.bat
@@ -2,17 +2,88 @@
 @rem Copyright (c) 2019. TIBCO Software Inc.
 @rem This file is subject to the license terms contained in the license file that is distributed with this file.
 
-if !IS_S2I! NEQ "false" (
-set USAGE="Usage: build_app_image.bat"
-call ..\s2i\create_builder_image.bat %* nos2i
-) else (
+setlocal EnableExtensions EnableDelayedExpansion
+
+REM Initializing variables
+set "ARG_APP_LOCATION=na"
+set "ARG_VERSION=na"
+set "ARG_IMAGE_VERSION=na"
+set "ARG_DOCKERFILE=na"
+set "ARG_GVPROVIDERS=na"
+set "TEMP_FOLDER=tmp_%RANDOM%"
+
+
+REM shift count to handle arg count>10
+set /A SHIFT_COUNT=7
+
+REM Getting argument count
+set argCount=0
+for %%x in (%*) do set /A argCount+=1
+
+REM Looping over all arguments and assigning variables
+set /A counter=0
+for /l %%x in (1, 1, %argCount%) do (
+  set /A counter=!counter!+1
+  call set currentArg=%%!counter!
+ 
+  if !currentArg! EQU -a (
+    set /a inCounter=!counter!+1
+    call set "ARG_APP_LOCATION=%%!inCounter!"
+	set "ARG_APP_LOCATION=!ARG_APP_LOCATION:"=!"	
+  )
+  if !currentArg! EQU --app-location (
+    set /a inCounter=!counter!+1
+    call set "ARG_APP_LOCATION=%%!inCounter!"
+	set "ARG_APP_LOCATION=!ARG_APP_LOCATION:"=!"
+  )
+  if !currentArg! EQU -r (
+    set /a inCounter=!counter!+1
+    call set "ARG_IMAGE_VERSION=%%!inCounter!"
+	set "ARG_IMAGE_VERSION=!ARG_IMAGE_VERSION:"=!"	
+  )
+  if !currentArg! EQU --repo (
+    set /a inCounter=!counter!+1
+    call set "ARG_IMAGE_VERSION=%%!inCounter!"
+	set "ARG_IMAGE_VERSION=!ARG_IMAGE_VERSION:"=!"
+  )
+  if !currentArg! EQU -d (
+    set /a inCounter=!counter!+1
+    call set "ARG_DOCKERFILE=%%!inCounter!" 
+	set "ARG_DOCKERFILE=!ARG_DOCKERFILE:"=!"
+  )
+  if !currentArg! EQU --docker-file (
+    set /a inCounter=!counter!+1
+    call set "ARG_DOCKERFILE=%%!inCounter!" 
+	set "ARG_DOCKERFILE=!ARG_DOCKERFILE:"=!"
+  )
+  if !currentArg! EQU -h (
+     call :printUsage
+     EXIT /B 1
+  )
+  if !currentArg! EQU --help (
+     call :printUsage
+     EXIT /B 1
+  )
+
+)
+
+
+REM Calling base image script 
+call ../base/build_base_image.bat %*
+
+set base_returned_params=%return_values%
+
+REM Setting the result paramaters to its values
+for %%a in (%base_returned_params%) do (        
+		for /F "tokens=1,2 delims=#" %%b in ("%%a") do (
+			set %%b=%%c
+			echo Setting %%b to !%%b!
+		)
+
+)
 
 REM Identify cdd and ear file names.
 set CDD_FILE_NAME=na
-
-echo ARG_VERSION: !ARG_VERSION!
-echo ARG_APP_LOCATION: !ARG_APP_LOCATION!
-
 for %%f in (!ARG_APP_LOCATION!\*.cdd) do (
   if !CDD_FILE_NAME! NEQ na (
     echo ERROR: Multiple cdd files found at the specified location.
@@ -37,14 +108,70 @@ if !EAR_FILE_NAME! EQU na (
   GOTO END-withError
 )
 
-mkdir !TEMP_FOLDER!\app
+REM Default Dockerfile depending on platform
+if "!ARG_DOCKERFILE!" EQU "na" if "!ARG_INSTALLERS_PLATFORM!" EQU "linux" set ARG_DOCKERFILE=Dockerfile
+if "!ARG_DOCKERFILE!" EQU "na" if "!ARG_INSTALLERS_PLATFORM!" EQU "win" set ARG_DOCKERFILE=Dockerfile.win
+if "!ARG_DOCKERFILE!" EQU "na" set ARG_DOCKERFILE=Dockerfile.win
+
+set /A RESULT=0
+set ARG_JRE_VERSION=na
+
+REM Identify JRE version
+for /F "tokens=1,2 delims==" %%a in ('findstr /i "BE_JRE_!ARG_VERSION!" ..\lib\docker_app_be.properties') do (        
+		set ARG_BE_JRE_VERSION=%%a
+		set ARG_JRE_VERSION=%%b
+		echo ARG_JRE_VERSION is set as !ARG_JRE_VERSION!
+
+)
+
+
+
+echo ----------------------------------------------
+echo INFO: Image Repo - !ARG_IMAGE_VERSION!
+echo INFO: Dockerfile - !ARG_DOCKERFILE!
 echo INFO: CDD file name - !CDD_FILE_NAME!
 echo INFO: EAR file name - !EAR_FILE_NAME!
-xcopy /Q /C /R /Y /E !ARG_APP_LOCATION!\* !TEMP_FOLDER!\app
+echo ----------------------------------------------
 
+mkdir !TEMP_FOLDER!\app
+
+set SHORT_VERSION=!ARG_VERSION:~0,3!
+
+echo INFO: Copying packages...
+
+xcopy /Q /C /R /Y /E !ARG_APP_LOCATION!\* !TEMP_FOLDER!\app
+xcopy /Q /C /R /Y /E ..\lib\docker_app_be.properties !TEMP_FOLDER!\app
+xcopy /Q /C /R /Y /E ..\lib\exclude_path.bat !TEMP_FOLDER!\app
+
+
+REM Identify Excluded elements
+set CURRENT_DIR=%cd%
+echo !CURRENT_DIR!
+set "EXCLUDE_STRING=na" 
+cd !TEMP_FOLDER!\app
+jar xvf !EAR_FILE_NAME!
+jar xvf "Shared Archive.sar"
+cd Channels
+for /F "tokens=1,2,3 delims==" %%a in ('findstr /i "DOCKER_CHK_CHANNEL" !CURRENT_DIR!\..\lib\docker_app_be.properties') do (        
+		set ARG_1=%%a
+		set ARG_2=%%b
+		set ARG_3=%%c
+		for /F %%i in ('dir /s/b "%%b*.channel"') do (
+		
+		if "!EXCLUDE_STRING!" NEQ "na" (set EXCLUDE_STRING=!EXCLUDE_STRING!,!SHORT_VERSION!\%%c)
+		if "!EXCLUDE_STRING!" EQU "na" (set EXCLUDE_STRING=!SHORT_VERSION!\%%c)
+		
+	)  
+)
+
+cd !CURRENT_DIR!
+
+echo Path to be excluded for this app ... [!EXCLUDE_STRING!]
+
+echo INFO: Building docker image for TIBCO BusinessEvents Version:!ARG_VERSION! and Image Repository:!ARG_IMAGE_VERSION! and Docker file:!ARG_DOCKERFILE!
 copy !ARG_DOCKERFILE! !TEMP_FOLDER!
 for %%f in (!ARG_DOCKERFILE!) do set ARG_DOCKERFILE=%%~nxf
-docker build -f !TEMP_FOLDER!\!ARG_DOCKERFILE! --build-arg BE_PRODUCT_VERSION="!ARG_VERSION!" --build-arg BE_SHORT_VERSION="!SHORT_VERSION!" --build-arg BE_PRODUCT_IMAGE_VERSION="!ARG_IMAGE_VERSION!" --build-arg BE_PRODUCT_ADDONS="!ARG_ADDONS!" --build-arg BE_PRODUCT_HOTFIX="!ARG_HF!" --build-arg AS_PRODUCT_HOTFIX="!ARG_AS_HF!" --build-arg DOCKERFILE_NAME=!ARG_DOCKERFILE! --build-arg AS_VERSION="!ARG_AS_VERSION!" --build-arg AS_SHORT_VERSION="!AS_SHORT_VERSION!" --build-arg JRE_VERSION=!ARG_JRE_VERSION! --build-arg TEMP_FOLDER=!TEMP_FOLDER! --build-arg CDD_FILE_NAME=!CDD_FILE_NAME! --build-arg EAR_FILE_NAME=!EAR_FILE_NAME! --build-arg GVPROVIDERS="!ARG_GVPROVIDERS!" -t "!ARG_IMAGE_VERSION!" !TEMP_FOLDER!
+docker build -f !TEMP_FOLDER!\!ARG_DOCKERFILE! --progress=tty --build-arg JRE_VERSION=!ARG_JRE_VERSION! --build-arg EXCLUDE_STRING="!EXCLUDE_STRING!" --build-arg BE_PRODUCT_VERSION="!ARG_VERSION!" --build-arg BE_SHORT_VERSION="!SHORT_VERSION!" --build-arg BE_PRODUCT_IMAGE_VERSION="!ARG_IMAGE_VERSION!" --build-arg DOCKERFILE_NAME=!ARG_DOCKERFILE! --build-arg TEMP_FOLDER=!TEMP_FOLDER! --build-arg CDD_FILE_NAME=!CDD_FILE_NAME! --build-arg EAR_FILE_NAME=!EAR_FILE_NAME! -t "!ARG_IMAGE_VERSION!" !TEMP_FOLDER!
 
 if %ERRORLEVEL% NEQ 0 (
   echo "Docker build failed."
@@ -56,7 +183,7 @@ echo Deleting temporary intermediate image..
 for /f "tokens=*" %%i IN ('docker images -q -f "label=be-intermediate-image=true"') do (
   docker rmi %%i
 )
-echo DONE: Docker build successful
+echo DONE: Docker build successful - '!ARG_IMAGE_VERSION!'
 
 :END
 if exist !TEMP_FOLDER! rmdir /S /Q "!TEMP_FOLDER!"
@@ -65,7 +192,18 @@ EXIT /B 0
 
 :END-withError
 if exist !TEMP_FOLDER! rmdir /S /Q "!TEMP_FOLDER!"
-if %ERRORLEVEL% NEQ 0 ( EXIT /B %ERRORLEVEL% )
 ENDLOCAL
+if %ERRORLEVEL% NEQ 0 ( EXIT /B %ERRORLEVEL% )
 EXIT /B 1
-)
+
+:printUsage 
+  echo Usage: build_app_image.bat
+  echo  [-l/--installers-location]  :       Location where TIBCO BusinessEvents and TIBCO Activespaces installers are located [required]
+  echo  [-a/--app-location]         :       Location where the application ear, cdd and other files are located [required]
+  echo  [-r/--repo]                 :       The app image Repository (example - fdc:latest) [required]
+  echo  [-d/--docker-file]          :       Dockerfile to be used for generating image (default - Dockerfile.win for windows container, Dockerfile for others) [optional]
+  echo  [--gv-providers]            :       Names of GV providers to be included in the image. Supported value - consul [optional]
+  echo  [-h/--help]                 :       Print the usage of script [optional]
+  echo.
+  echo  NOTE: Encapsulate all the arguments between double quotes
+EXIT /B 0
--- cloud/docker/bin/build_rms_image.bat
+++ cloud/docker/bin/build_rms_image.bat
@@ -5,14 +5,14 @@
 setlocal EnableExtensions EnableDelayedExpansion
 
 REM Initializing variables
-set "ARG_INSTALLER_LOCATION=na"
 set "ARG_APP_LOCATION=na"
 set "ARG_VERSION=na"
 set "ARG_IMAGE_VERSION=na"
 set "ARG_HF=na"
-set "ARG_AS_HF=na"
 set "ARG_DOCKERFILE=na"
+set "ARG_GVPROVIDERS=na"
 set "TEMP_FOLDER=tmp_%RANDOM%"
+set ARG_INSTALLERS_PLATFORM=win
 
 REM shift count to handle arg count>10
 set /A SHIFT_COUNT=7
@@ -27,17 +27,7 @@ for /l %%x in (1, 1, %argCount%) do (
   set /A counter=!counter!+1
   call set currentArg=%%!counter!
  
-  if !currentArg! EQU -l (
-    set /a inCounter=!counter!+1
-    call set "ARG_INSTALLER_LOCATION=%%!inCounter!" 
-	set "ARG_INSTALLER_LOCATION=!ARG_INSTALLER_LOCATION:"=!"	
-  )
-  if !currentArg! EQU --installers-location (
-    set /a inCounter=!counter!+1
-    call set "ARG_INSTALLER_LOCATION=%%!inCounter!"
-	set "ARG_INSTALLER_LOCATION=!ARG_INSTALLER_LOCATION:"=!"	
-  )
-  if !currentArg! EQU -a (
+ if !currentArg! EQU -a (
     set /a inCounter=!counter!+1
     call set "ARG_APP_LOCATION=%%!inCounter!"
 	set "ARG_APP_LOCATION=!ARG_APP_LOCATION:"=!"	
@@ -77,96 +67,56 @@ for /l %%x in (1, 1, %argCount%) do (
   )
 )
 
-set "MISSING_ARG=-"
-REM Validating mandatory arguments
-if !ARG_INSTALLER_LOCATION! EQU na (
-  set "MISSING_ARG=!MISSING_ARG! Installer Location[-l/--installers-location] "
+
+REM Calling base image script 
+call ../base/build_base_image.bat %*
+
+
+set base_returned_params=%return_values%
+
+REM Setting the result paramaters to its values
+for %%a in (%base_returned_params%) do (        
+		for /F "tokens=1,2 delims=#" %%b in ("%%a") do (
+			set %%b=%%c
+			echo Setting %%b to !%%b!
+		)
+
 )
 
-if !MISSING_ARG! NEQ - (
-  echo ERROR: Missing mandatory argument^(s^) : !MISSING_ARG!
-  call :printUsage
-  GOTO END-withError
-)
-
-REM Checking if the specified directory exists or not
-if NOT EXIST !ARG_INSTALLER_LOCATION! (
-  echo ERROR: The directory - !ARG_INSTALLER_LOCATION! is not a valid directory. Enter a valid directory and try again.
-  GOTO END-withError
-)
-
-REM Identify Installers platform (linux, win)
-set ARG_INSTALLERS_PLATFORM=na
-for %%f in (!ARG_INSTALLER_LOCATION!\*linux*.zip) do (
-  set ARG_INSTALLERS_PLATFORM=linux
-)
-for %%f in (!ARG_INSTALLER_LOCATION!\*win*.zip) do (
-  if !ARG_INSTALLERS_PLATFORM! EQU linux (
-    echo ERROR: Installers for multiple platforms found at the specified location.
-    GOTO END-withError
-  )
-  set ARG_INSTALLERS_PLATFORM=win
-)
 
 REM Default Dockerfile depending on platform
 if "!ARG_DOCKERFILE!" EQU "na" if "!ARG_INSTALLERS_PLATFORM!" EQU "linux" set ARG_DOCKERFILE=Dockerfile-rms
 if "!ARG_DOCKERFILE!" EQU "na" if "!ARG_INSTALLERS_PLATFORM!" EQU "win" set ARG_DOCKERFILE=Dockerfile-rms.win
-if "!ARG_DOCKERFILE!" EQU "na" set ARG_DOCKERFILE=Dockerfile-rms
+if "!ARG_DOCKERFILE!" EQU "na" set ARG_DOCKERFILE=Dockerfile-rms.win
 
 set /A RESULT=0
 set ARG_JRE_VERSION=na
-set ARG_AS_VERSION=na
 
-mkdir !TEMP_FOLDER!\installers !TEMP_FOLDER!\lib !TEMP_FOLDER!\app
+mkdir  !TEMP_FOLDER!\app
 break>"!TEMP_FOLDER!\app\rms_files"
-
-REM Performing validation
-call ..\lib\be_validate_installers.bat ARG_VERSION !ARG_INSTALLER_LOCATION! !TEMP_FOLDER! false true ARG_HF ARG_ADDONS ARG_AS_VERSION ARG_AS_HF ARG_JRE_VERSION
-if %ERRORLEVEL% NEQ 0 (
-  echo Docker build failed.
-  GOTO END-withError
-)
-
-if "!ARG_AS_VERSION!" EQU "na" (
-  echo ERROR: TIBCO Activespaces is required for RMS
-  GOTO END-withError
-)
 
 if "!ARG_IMAGE_VERSION!" EQU "na" (
   set "ARG_IMAGE_VERSION=rms:!ARG_VERSION!"
 )
 
 echo ----------------------------------------------
-echo INFO: Installers Location - !ARG_INSTALLER_LOCATION!
-echo INFO: Installers Platform - !ARG_INSTALLERS_PLATFORM!
-echo INGO: BusinessEvents version - !ARG_VERSION!
-echo INFO: BusinessEvents HF - !ARG_HF!
-echo INFO: ActiveSpaces version - !ARG_AS_VERSION!
-echo INFO: ActiveSpaces Hf - !ARG_AS_HF!
 echo INFO: Image Repo - !ARG_IMAGE_VERSION!
 echo INFO: Dockerfile - !ARG_DOCKERFILE!
 echo INFO: RMS Ear/Cdd Location - !ARG_APP_LOCATION!
 echo ----------------------------------------------
 
 echo INFO: Copying packages...
-for /F "tokens=*" %%f in  (!TEMP_FOLDER!/package_files.txt) do (
-  set FILE=%%f
-  SET FILE_PATH=!FILE:*#=!
-  xcopy /Q /C /R /Y !FILE_PATH! !TEMP_FOLDER!\installers
-)
 
 set SHORT_VERSION=!ARG_VERSION:~0,3!
-set AS_SHORT_VERSION=!ARG_AS_VERSION:~0,3!
-xcopy /Q /C /R /Y ..\lib !TEMP_FOLDER!\lib
+
 if !ARG_APP_LOCATION! NEQ na (
-  xcopy /Q /C /R /Y !ARG_APP_LOCATION!\*.cdd !TEMP_FOLDER!\app
-  xcopy /Q /C /R /Y !ARG_APP_LOCATION!\*.ear !TEMP_FOLDER!\app
+  xcopy /Q /C /R /Y !ARG_APP_LOCATION!\* !TEMP_FOLDER!\app
 )
 
-echo INFO: Building docker image for TIBCO BusinessEvents Version:!ARG_VERSION! and Image Repository:!ARG_IMAGE_VERSION! and Docker file:!ARG_DOCKERFILE!
+echo INFO: Building Application docker image for TIBCO BusinessEvents Version:!ARG_VERSION! and Image Repository:!ARG_IMAGE_VERSION! and Docker file:!ARG_DOCKERFILE!
 copy !ARG_DOCKERFILE! !TEMP_FOLDER!
 for %%f in (!ARG_DOCKERFILE!) do set ARG_DOCKERFILE=%%~nxf
-docker build -f !TEMP_FOLDER!\!ARG_DOCKERFILE! --build-arg BE_PRODUCT_VERSION="!ARG_VERSION!" --build-arg BE_SHORT_VERSION="!SHORT_VERSION!" --build-arg BE_PRODUCT_IMAGE_VERSION="!ARG_IMAGE_VERSION!" --build-arg BE_PRODUCT_ADDONS="!ARG_ADDONS!" --build-arg BE_PRODUCT_HOTFIX="!ARG_HF!" --build-arg AS_PRODUCT_HOTFIX="!ARG_AS_HF!" --build-arg DOCKERFILE_NAME=!ARG_DOCKERFILE! --build-arg AS_VERSION="!ARG_AS_VERSION!" --build-arg AS_SHORT_VERSION="!AS_SHORT_VERSION!" --build-arg JRE_VERSION=!ARG_JRE_VERSION! --build-arg TEMP_FOLDER=!TEMP_FOLDER! -t "!ARG_IMAGE_VERSION!" !TEMP_FOLDER!
+docker build -f !TEMP_FOLDER!\!ARG_DOCKERFILE! --build-arg BE_PRODUCT_VERSION="!ARG_VERSION!" --build-arg BE_SHORT_VERSION="!SHORT_VERSION!" --build-arg BE_PRODUCT_IMAGE_VERSION="!ARG_IMAGE_VERSION!" --build-arg BE_PRODUCT_ADDONS="!ARG_ADDONS!" --build-arg BE_PRODUCT_HOTFIX="!ARG_HF!" --build-arg DOCKERFILE_NAME=!ARG_DOCKERFILE! --build-arg JRE_VERSION=!ARG_JRE_VERSION! --build-arg TEMP_FOLDER=!TEMP_FOLDER! -t "!ARG_IMAGE_VERSION!" !TEMP_FOLDER!
 
 if %ERRORLEVEL% NEQ 0 (
   echo Docker build failed.
