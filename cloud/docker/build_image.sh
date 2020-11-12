0879ccd739845ce181a355e045c4ff28f8d6a6bd
nthota <nthota@tibco.com>
2020-11-12 11:42:18 +0530

    tests updated with master

--- cloud/docker/build_image.sh
+++ cloud/docker/build_image.sh
@@ -763,6 +763,6 @@ if [ $BUILD_SUCCESS = "true" ]; then
     # docker unit tests
     if [[ ($ARG_ENABLE_TESTS = "true") && (("$IMAGE_NAME" = "$BUILDER_IMAGE") || ("$IMAGE_NAME" = "$APP_IMAGE")) ]]; then
         cd ./tests
-        source run_tests.sh -i $ARG_IMAGE_VERSION  -b $ARG_BE_SHORT_VERSION -c $CDD_FILE_NAME -e $EAR_FILE_NAME -al $ARG_AS_LEG_SHORT_VERSION -as $ARG_AS_SHORT_VERSION -f $ARG_FTL_SHORT_VERSION
+        source run_tests.sh -i $ARG_IMAGE_VERSION  -b $ARG_BE_SHORT_VERSION -al $ARG_AS_LEG_SHORT_VERSION -as $ARG_AS_SHORT_VERSION -f $ARG_FTL_SHORT_VERSION
     fi
 fi
