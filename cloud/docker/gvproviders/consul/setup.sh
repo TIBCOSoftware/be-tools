86bc77d1e8ae9ff9df9680b576c205da1246999c
nthota <nthota@tibco.com>
2021-03-12 17:55:20 +0530

    updating gv provider setup logic

--- cloud/docker/gvproviders/consul/setup.sh
+++ cloud/docker/gvproviders/consul/setup.sh
@@ -5,9 +5,23 @@
 # This file is subject to the license terms contained in the license file that is distributed with this file.
 #
 
-cd /home/tibco/be/gvproviders/consul
+source /home/tibco/be/gvproviders/gvutils.sh
 
-package-manager install -y unzip
+# fill this variable with required packages to be installed. Multiple packages can be represented in a space separated format ex: "curl unzip".
+REQUIRED_PKGS="unzip"
+
+# fill this variable with packages used only during build time. Multiple packages can be represented in a space separated format ex: "curl unzip".
+BUILDTIME_PKGS="unzip"
+
+INSTALL_PKGS_LIST=$( getInstallPkgs "$REQUIRED_PKGS" )
+CLEANUP_PKGS_LIST=$( getCleanupPkgs "$BUILDTIME_PKGS" "$INSTALL_PKGS_LIST" )
+
+# installing required packages
+if [ "$INSTALL_PKGS_LIST" != "" ]; then
+    package-manager install -y $INSTALL_PKGS_LIST
+fi
+
+cd /home/tibco/be/gvproviders/consul
 
 # Download consul cli and extract it.
 wget "https://releases.hashicorp.com/consul/1.6.1/consul_1.6.1_linux_amd64.zip"
@@ -15,4 +29,7 @@ unzip consul_1.6.1_linux_amd64.zip
 
 # clean up
 rm -f consul_1.6.1_linux_amd64.zip
-package-manager remove -y unzip
+
+if [ "$CLEANUP_PKGS_LIST" != "" ]; then
+    package-manager remove -y $CLEANUP_PKGS_LIST
+fi
