78a22f806e5a651fd31c1960ceb019cf6dac1d8a
nthota <nthota@tibco.com>
2021-07-12 10:57:21 +0530

    teagent shell file updated

--- cloud/docker/lib/run_teagent.sh
+++ cloud/docker/lib/run_teagent.sh
@@ -12,11 +12,11 @@ if [[ "$9" == "aws"  || "$9" == "fargate" ]]; then
   && aws configure set default.output ${13}
 fi
 
-AWS_CONFIG_FILE="/root/.aws/config"
-echo "role_arn = ${14}" >> $AWS_CONFIG_FILE
-echo "source_profile=default" >>$AWS_CONFIG_FILE
-
 if [[ "$9" == "aws" ]]; then
+  AWS_CONFIG_FILE="/root/.aws/config"
+  echo "role_arn = ${14}" >> $AWS_CONFIG_FILE
+  echo "source_profile=default" >>$AWS_CONFIG_FILE
+
   nohup python3 be_docker_container_discovery_aws.py  -t $3 -u $4 -p $5 -py $6 -pi $7 -s $8 > discovery.logs 2>&1 &
 elif [[ "$9" == "fargate" ]]; then
   nohup python3 be_docker_container_discovery_fargate.py  -t $3 -u $4 -p $5 -py $6 -pi $7 -c $8 > discovery.logs 2>&1 &
