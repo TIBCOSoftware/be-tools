0341d83f467f482e550b409c75511b35a4bfd9c3
LakshmiMekala <lmekala@tibco.com>
2021-08-13 17:06:42 +0530

    add,update pv and pvc tests

--- cloud/kubernetes/tests/template/pv_manifest_test.go
+++ cloud/kubernetes/tests/template/pv_manifest_test.go
@@ -175,3 +175,63 @@ func TestPVManifestMinikube(t *testing.T) {
 	require.Equal(t, v1.HostPathType("DirectoryOrCreate"), *actualPVs[0].Spec.HostPath.Type)
 	require.Equal(t, "/volume1/data-store", actualPVs[0].Spec.HostPath.Path)
 }
+
+func TestPVManifestSNLogs(t *testing.T) {
+	helmChartPath, err := filepath.Abs("../../helm")
+	releaseName := "TestPVManifestSNLogs"
+	require.NoError(t, err)
+
+	// TC: minikube, sharedNothing, persitence enbled (for logs)
+	values := map[string]string{
+		"bsType":                   "sharednothing",
+		"persistence.logs":         "true",
+		"persistence.storageClass": "-",
+	}
+	options := &helm.Options{
+		SetValues: values,
+	}
+	output, err := helm.RenderTemplateE(t, options, helmChartPath, releaseName, []string{"templates/pv-manifest.yaml"})
+	require.NoError(t, err)
+	rawPVs := strings.Split(output, "---")
+	actualPVs := []v1.PersistentVolume{}
+	for _, rawPV := range rawPVs {
+		if strings.Trim(rawPV, "") == "" {
+			continue
+		}
+		var PV v1.PersistentVolume
+		helm.UnmarshalK8SYaml(t, rawPV, &PV)
+		actualPVs = append(actualPVs, PV)
+	}
+	expectedPVsMap := map[string]map[string]interface{}{
+		"TestPVManifestSNLogs-data-store": {
+			"releaseName":  "TestPVManifestSNLogs-data-store",
+			"memoryQty":    "512Mi",
+			"scName":       "",
+			"claimRefName": "TestPVManifestSNLogs-data-store",
+			"path":         "/volume1/data-store",
+			"type":         "DirectoryOrCreate",
+		},
+		"TestPVManifestSNLogs-logs": {
+			"releaseName":  "TestPVManifestSNLogs-logs",
+			"memoryQty":    "512Mi",
+			"scName":       "",
+			"claimRefName": "TestPVManifestSNLogs-logs",
+			"path":         "/volume1/logs",
+			"type":         "DirectoryOrCreate",
+		},
+	}
+	require.Equal(t, 2, len(actualPVs))
+
+	for _, actualPV := range actualPVs {
+		actualPVName := actualPV.ObjectMeta.Name
+		expectedPV, found := expectedPVsMap[actualPVName]
+		require.Truef(t, found, fmt.Sprintf("PV name[%s] is not expected", actualPVName))
+		require.Equal(t, expectedPV["releaseName"], actualPV.ObjectMeta.Name)
+		actualMemQty := actualPV.Spec.Capacity["storage"]
+		require.Equal(t, expectedPV["memoryQty"], actualMemQty.String())
+		require.Equal(t, expectedPV["scName"], actualPV.Spec.StorageClassName)
+		require.Equal(t, expectedPV["claimRefName"], actualPV.Spec.ClaimRef.Name)
+		require.Equal(t, v1.HostPathType(fmt.Sprintf("%s", expectedPV["type"])), *actualPV.Spec.HostPath.Type)
+		require.Equal(t, expectedPV["path"], actualPV.Spec.HostPath.Path)
+	}
+}
