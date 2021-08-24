d81267f6b5b2a9e2fd5ba823ab8e0513edc1d48c
ykalidin <ykalidin@tibco.com>
2021-08-24 08:04:09 +0530

    Update teagent files

--- cloud/docker/lib/run_teagent.sh
+++ cloud/docker/lib/run_teagent.sh
@@ -14,32 +14,29 @@ if [[ ! -z "$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI" ]]; then
     exit 1
   fi
 
-  ecs_metadata=$(curl 169.254.170.2/v2/metadata)
+  ecs_metadata=$(curl ${ECS_CONTAINER_METADATA_URI_V4}/task)
+  echo $ecs_metadata
   cluster=$(echo "$ecs_metadata" | jq -r ".Cluster" | rev | cut -d '/' -f 1 | rev )
-  availabilityZone=$(echo "$ecs_metadata" | jq -r ".AvailabilityZone" | rev | cut -c2- | rev)
   output="json"
 
-  echo $cluster $availabilityZone $output 
+  echo $cluster $output 
   if [ "$8" != "nil" ]; then
     echo $8
 	  cluster="$8"
 	fi
-  if [ "${10}" != "nil" ]; then
-    echo ${10}
-	  availabilityZone="${10}"
-	fi
+
   if [ "${11}" != "nil" ]; then
     echo ${11}
 	  output="${11}"
 	fi  
 
-  echo $cluster $availabilityZone $output 
+  echo $cluster $output 
 fi
 
 if [[ "$9" == "AWS_ECS_FARGATE"  || "$9" == "AWS_ECS_EC2" ]]; then
   aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" \
   && aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" \
-  && aws configure set default.region $availabilityZone \
+  && aws configure set default.region ${10} \
   && aws configure set default.output ${11}
   aws configure set aws_session_token $AWS_SESSION_TOKEN
 fi
@@ -55,6 +52,8 @@ if [[ "$9" == "AWS_ECS_FARGATE"  || "$9" == "AWS_ECS_EC2" ]]; then
   echo $3 $4 $5 $6 $7 $clustername
   nohup python3 be_docker_container_discovery_aws_ecs.py  -t $3 -u $4 -p $5 -py $6 -pi $7 -c $cluster -lt $launchType > discovery.logs 2>&1 &
 elif [[ "$9" == "k8s" ]]; then
+  echo "IN be_docker_container_discovery_k8s"
+  echo $3 $4 $5 $6 $7 $8
   nohup python3 be_docker_container_discovery_k8s.py  -t $3 -u $4 -p $5 -py $6 -pi $7 -ta $8 > discovery.logs 2>&1 &
 fi
 $1/teagent/bin/be-teagent --propFile $2
