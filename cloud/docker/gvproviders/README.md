507cbc122b85a25e121a60751fcb035a8bb1d91d
yogita <ykalidin@tibco.com>
2022-10-28 09:58:46 +0530

    Updating new changes

--- cloud/docker/gvproviders/README.md
+++ cloud/docker/gvproviders/README.md
@@ -7,17 +7,13 @@ TIBCO BusinessEvents® documentation is available at [Using GV Configuration Fra
 
 ## Cyberark Conjur GV Provider
 
-## Prerequisites
+## Procedure
 
-*  The Conjur server with all policies and variables loaded.
+*  You need Conjur server with all policies and variables loaded.Get the conjur details, such as server url,account name, your login from conjur admin.
 *  (Optional) For using conjur cli in secured mode, ensure that you have access to the CA certificate.
 
-Note: You may use [Quickstart](https://www.conjur.org/get-started/quick-start/oss-environment/) for basic setup of conjur using docker images in local. 
+Note: You may use [Quickstart](https://www.conjur.org/get-started/quick-start/oss-environment/) for basic setup of conjur server and client using docker images in local. 
 Refer Unit 1-For setting up conjur server, Unit 2-Change the policies according to your variables and Unit 3-Load the variables mentioned in the policies. Use the `my_app_data` file generated for CONJUR_LOGIN and CONJUR_APIKEY.
-
-### Procedure
-1. For existing Conjur server, get the conjur details from conjur admin.
-2. \(Optional\) For the secured conjur connection, copy the CA certificate in the same folder as application EAR and CDD files.
 
 ### Build
 To select this provider type, pass `conjur` to --gv-provider flag while building the BE application image.
@@ -30,25 +26,24 @@ Sample:
 -t fdconjur:latest
 ```
 
-###Conjur Run
+### Conjur Run
 Following environment variables are applicable for this GV provider type:
 * CONJUR_SERVER_URL - Conjur Server URL
 * CONJUR_ACCOUNT - Account created in conjur
 * CONJUR_LOGIN - User or host name
 * CONJUR_APIKEY - Api key
+* CONJUR_SECURE - Set value to `true` to run conjur cli in secure mode.Also copy the CA certificate in the same folder as application EAR and CDD files.
 
 Sample run:
 ```sh
 docker run \
--e "CONJUR_SERVER_URL=http://consul:8500" \
+-e "CONJUR_SERVER_URL=https://conjur:8500" \
 -e "CONJUR_ACCOUNT=<Conjur account>" \
 -e "CONJUR_LOGIN=<Conjur user or host>" \
 -e "CONJUR_APIKEY=<api_key>" \
 -p 8108:8108 --name=fdconjur fdconjur:latest
 ```
-Sample run(Secured Conjur Server):TBD
-
-The `CONJUR_CACERT` environment variable is only required for the secured mode.
+Sample run(Secured Conjur Server):
 
 ```sh
 docker run \
@@ -56,6 +51,7 @@ docker run \
 -e CONJUR_SERVER_URL=https://proxy \
 -e CONJUR_LOGIN=admin \
 -e CONJUR_APIKEY=2jtsx0c358kcyw15a0rm112d4b1e5yfsm11eeah7q6n0dx61t8cr57 \
+-e CONJUR_SECURE=true
 --network conjur-quickstart_default
 -p 8108:8108 --name=bot_app fdconsul:latest
 ```
--- cloud/docker/gvproviders/cyberark/run.sh
+++ cloud/docker/gvproviders/cyberark/run.sh
@@ -5,6 +5,8 @@
 # This file is subject to the license terms contained in the license file that is distributed with this file.
 #
 
+echo "Configuring Conjur"
+
 if [ -z "$CONJUR_SERVER_URL" ]; then
   echo "WARN: GV provider[conjur] is configured but env variable CONJUR_SERVER_URL is empty OR not supplied."
   echo "WARN: Skip fetching GV values from Conjur."
@@ -32,24 +34,48 @@ fi
 touch /home/tibco/be/gvproviders/output.json
 JSON_FILE=/home/tibco/be/gvproviders/output.json
 
-#Initialize the conjur cli
-conjur init -a $CONJUR_ACCOUNT -u $CONJUR_SERVER_URL -c /opt/tibco/be/ext/*
-
-#Authenticate to conjur
-conjur login -i $CONJUR_LOGIN -p $CONJUR_APIKEY
-
-#Fetch variable list
-variablelist=$(conjur list --kind variable)
 echo "{" >> $JSON_FILE
 
-for variable in $(echo $variablelist | sed 's/[][]//g')
-do
-    variable="${variable//\"}"         ##Remove "" myConjurAccount:variable:BotApp/secretVar,
-    variable="${variable//,}"          ##Remove ,  myConjurAccount:variable:BotApp/IGNITE/DISCOVERY_URL
-    variable=${variable##*:}           ##Get variable  BotApp/IGNITE/DISCOVERY_URL
-    value=$(conjur variable get -i $variable)
-    echo -e "  \"${variable#*/}\": \"$value\"," >> $JSON_FILE  #IGNITE/DISCOVERY_URL
-done
+if [ $CONJUR_SECURE ]
+then
+    #Initialize the conjur cli
+    conjur init -a $CONJUR_ACCOUNT -u $CONJUR_SERVER_URL -c /opt/tibco/be/ext/*
+    
+    #Authenticate to conjur
+    conjur login -i $CONJUR_LOGIN -p $CONJUR_APIKEY
+    
+    #Fetch variable list
+    variablelist=$(conjur list --kind variable)
+    
+    for variable in $(echo $variablelist | sed 's/[][]//g')
+    do
+        variable="${variable//\"}"         ##Remove "" myConjurAccount:variable:BotApp/secretVar,
+        variable="${variable//,}"          ##Remove ,  myConjurAccount:variable:BotApp/IGNITE/DISCOVERY_URL
+        variable=${variable##*:}           ##Get variable  BotApp/IGNITE/DISCOVERY_URL
+        value=$(conjur variable get -i $variable)
+        echo -e "  \"${variable#*/}\": \"$value\"," >> $JSON_FILE  #IGNITE/DISCOVERY_URL
+    done
+else
+    #Initialize the conjur cli
+    conjur --insecure init -a $CONJUR_ACCOUNT -u $CONJUR_SERVER_URL
+    
+    #Authenticate to conjur
+    conjur --insecure login -i $CONJUR_LOGIN -p $CONJUR_APIKEY
+    
+    #Fetch variable list
+    variablelist=$(conjur --insecure list --kind variable)
+    
+    for variable in $(echo $variablelist | sed 's/[][]//g')
+    do
+        variable="${variable//\"}"         ##Remove "" myConjurAccount:variable:BotApp/secretVar,
+        variable="${variable//,}"          ##Remove ,  myConjurAccount:variable:BotApp/IGNITE/DISCOVERY_URL
+        variable=${variable##*:}           ##Get variable  BotApp/IGNITE/DISCOVERY_URL
+        value=$(conjur --insecure variable get -i $variable)
+        echo -e "  \"${variable#*/}\": \"$value\"," >> $JSON_FILE  #IGNITE/DISCOVERY_URL
+    done
+fi
+
+conjur logout
 
 sed -i '$ s/.$//' $JSON_FILE
 echo "}" >> $JSON_FILE
