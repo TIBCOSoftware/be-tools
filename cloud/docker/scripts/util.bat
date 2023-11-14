3509bfc955c8cdaadf845d0254ea4925a055586c
nareshkumarthota <nthota@tibco.com>
2023-11-14 22:02:19 +0530

    windows scripts updated

--- cloud/docker/scripts/util.bat
+++ cloud/docker/scripts/util.bat
@@ -167,44 +167,6 @@ GOTO :EOF
     SET "%~8=!BASE_INST_VERSION!" & SET "%~9=!INSTLR_HF_VERSION!"
     EXIT /B 0
     
-:IdentifyInstaller
-    set INSTLR_LOCATION=%~1
-    set INSTLR_REG=%~2
-    set INSTLR_FULL_NAME=%~3
-    set INSTLR_HF=%4
-    set INSTLR_VERSION=na
-    set FILENAME=na
-    set MULTIPLE_INSTLRS=false
-
-    for /f %%i in ('dir /b !INSTLR_LOCATION! ^| findstr /I "!INSTLR_REG!"') do (
-        if !MULTIPLE_INSTLRS! EQU true (
-            echo ERROR: Multiple !INSTLR_FULL_NAME! installer found at the specified location:[!INSTLR_LOCATION!].
-            SET "%~5=!INSTLR_VERSION!" & SET "%~7=!MULTIPLE_INSTLRS!"
-            EXIT /B 1
-        )
-        set MULTIPLE_INSTLRS=true
-        set FILENAME=%%i
-        set FILENAME_SPLIT=!FILENAME:_= !
-        set /a indx=0
-        for %%j in (!FILENAME_SPLIT!) do (
-            set /a indx += 1
-            if !indx! EQU 3 (
-                set INSTLR_VERSION=%%j
-            )
-            if !INSTLR_HF! EQU true (
-                if !indx! EQU 4 (
-                    set INSTLR_VERSION=%%j
-                    set INSTLR_VERSION=!INSTLR_VERSION:.zip=!
-                    set INSTLR_VERSION=!INSTLR_VERSION:HF-=!
-                )
-            )
-        )
-    )
-    echo "INSTLR_VERSION"!INSTLR_VERSION!
-    echo "FILENAME"!FILENAME!
-    SET "%~5=!INSTLR_VERSION!" & SET "%~6=!FILENAME!" & SET "%~7=false"
-    EXIT /B 0
-
 :END-withError
     SET "%9=true"
     EXIT /B 1
