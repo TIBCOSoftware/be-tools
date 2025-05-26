e071fbb1c89c19036580a662aad6f2d8b5b456c5
nareshkumarthota <nthota@tibco.com>
2025-05-26 22:11:54 +0530

    added be 640 compatible changes

--- cloud/docker/scripts/hawk.sh
+++ cloud/docker/scripts/hawk.sh
@@ -1,6 +1,6 @@
 #supported HAWK versions for be version
-HAWK_VERSION_MAP_MIN=( "6.2.2:7.1.0" "6.3.0:7.1.0" "6.3.1:7.1.0" "6.3.2:7.1.0" )
-HAWK_VERSION_MAP_MAX=( "6.2.2:7.1.0" "6.3.0:7.2.1" "6.3.1:7.2.2" "6.3.2:7.3.0" )
+HAWK_VERSION_MAP_MIN=( "6.2.2:7.1.0" "6.3.0:7.1.0" "6.3.1:7.1.0" "6.3.2:7.1.0"  "6.4.0:7.1.0" )
+HAWK_VERSION_MAP_MAX=( "6.2.2:7.1.0" "6.3.0:7.2.1" "6.3.1:7.2.2" "6.3.2:7.3.0"  "6.4.0:7.3.0" )
 
 hawkMinVersion=$(echo $( getFromArray "$ARG_BE_VERSION" "${HAWK_VERSION_MAP_MIN[@]}" ) | sed -e "s/x/9/g" )
 hawkMaxVersion=$(echo $( getFromArray "$ARG_BE_VERSION" "${HAWK_VERSION_MAP_MAX[@]}" ) | sed -e "s/x/9/g" )
