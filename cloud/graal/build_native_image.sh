514cb361d61a2e6c3be5c02892b9233082331a56
nareshkumarthota <nthota@tibco.com>
2025-06-09 11:31:30 +0000

    changes for 640 added

--- cloud/graal/build_native_image.sh
+++ cloud/graal/build_native_image.sh
@@ -3,13 +3,20 @@
 BUILD_OPTION=$1
 PSP=:
 
-BE_HOME=/home/naresh/tibco/be/6.3
-BE_APP_VER=630
+BE_HOME=/home/nthota/tibco640/be/6.4
+BE_APP_VER=640
+
+# BE_HOME=/home/nthota/tibco/be/6.3
+# BE_APP_VER=632
+
+# BE_HOME=/home/nthota/tibco630/be/6.3
+# BE_APP_VER=630
+
 BE_APP_NAME=inmem
 
-BE_APP_HOME=/home/naresh/be-tools/cloud/graal/beapps/$BE_APP_VER/$BE_APP_NAME
+BE_APP_HOME=/home/nthota/git/be-tools/cloud/graal/beapps/$BE_APP_VER/$BE_APP_NAME
 META_DIR=$BE_APP_HOME/META-INF/native-image
-EXT_JARS_PATH=/home/naresh/be-tools/cloud/graal/extjars
+EXT_JARS_PATH=/home/nthota/git/be-tools/cloud/graal/extjars
 
 allJarsInPath(){
     DIRPATH=$1
@@ -47,17 +54,35 @@ rm bin/excludejars.txt*
 
 echo CP_PATH $CP_PATH
 
+export LD_LIBRARY_PATH=$BE_HOME/lib:$LD_LIBRARY_PATH:$BE_HOME/lib/ext/tpcl:$BE_HOME/lib/ext/tibco:$BE_HOME/lib/ext/apache
+
 if [[ "$BUILD_OPTION" == "" || "$BUILD_OPTION" == "1" ]]; then
     echo building native image $BE_APP_NAME$BE_APP_VER
-    native-image -classpath "$CP_PATH" com.tibco.cep.container.standalone.BEMain --no-fallback --enable-url-protocols=http -H:ConfigurationFileDirectories=$META_DIR -o bin/$BE_APP_NAME$BE_APP_VER --initialize-at-run-time=org.apache.logging.log4j,org.apache.logging.slf4j,io.netty --initialize-at-build-time=com.datastax.oss.driver.internal.core.util.Dependency
+    native-image \
+    --enable-url-protocols=https \
+    --enable-url-protocols=http \
+    --add-exports=java.base/sun.net.util=ALL-UNNAMED \
+    -classpath "$CP_PATH" com.tibco.cep.container.standalone.BEMain --no-fallback --enable-url-protocols=http -H:ConfigurationFileDirectories=$META_DIR -o bin/$BE_APP_NAME$BE_APP_VER \
+    --initialize-at-run-time=org.apache.logging.log4j,org.apache.logging.slf4j,io.netty \
+    --initialize-at-build-time=com.datastax.oss.driver.internal.core.util.Dependency,com.sun.xml.xsom.impl.AnnotationImpl\$LocatorImplUnmodifiable,com.datastax.oss.driver.shaded.guava.common.collect.RegularImmutableList,com.datastax.oss.driver.shaded.guava.common.collect.SingletonImmutableList
 fi
 
 if [ "$BUILD_OPTION" = "2" ]; then
     echo creating metadata 
-    java -agentlib:native-image-agent=config-output-dir=$META_DIR -classpath "$CP_PATH" com.tibco.cep.container.standalone.BEMain --propFile $BE_HOME/bin/be-engine.tra -u default -c "$BE_APP_HOME/cddear/fd.cdd" "$BE_APP_HOME/cddear/fd.ear"
+    # $BE_HOME/../../tibcojre64/21/bin/java \
+    # -DLD_LIBRARY_PATH=$LD_LIBRARY_PATH  \
+    # -Dlicense.license_source=https://gasdbeqa02.dev.tibco.com:7070/?fp=65de63b9b2edd62fc46404451e539a6e17265ecb658749daf22c3dd355306227  \
+    # -classpath "$CP_PATH" com.tibco.cep.container.standalone.BEMain --propFile $BE_HOME/bin/be-engine.tra -u default -c "$BE_APP_HOME/cddear/fd.cdd" "$BE_APP_HOME/cddear/fd.ear"
+
+    java \
+    -DLD_LIBRARY_PATH=$LD_LIBRARY_PATH  \
+    -Dlicense.license_source=https://gasdbeqa02.dev.tibco.com:7070/?fp=65de63b9b2edd62fc46404451e539a6e17265ecb658749daf22c3dd355306227  \
+    -agentlib:native-image-agent=config-output-dir=$META_DIR -classpath "$CP_PATH" com.tibco.cep.container.standalone.BEMain --propFile $BE_HOME/bin/be-engine.tra -u default -c "$BE_APP_HOME/cddear/fd.cdd" "$BE_APP_HOME/cddear/fd.ear"
 fi
 
 if [ "$BUILD_OPTION" = "3" ]; then
     echo running native image $BE_APP_NAME$BE_APP_VER
-    ./bin/$BE_APP_NAME$BE_APP_VER --propFile $BE_HOME/bin/be-engine.tra -u default -c "$BE_APP_HOME/cddear/fd.cdd" "$BE_APP_HOME/cddear/fd.ear"
+    ./bin/$BE_APP_NAME$BE_APP_VER \
+    -DLD_LIBRARY_PATH=$LD_LIBRARY_PATH  \
+    -Dlicense.license_source=https://gasdbeqa02.dev.tibco.com:7070/?fp=65de63b9b2edd62fc46404451e539a6e17265ecb658749daf22c3dd355306227 --propFile $BE_HOME/bin/be-engine.tra -u default -c "$BE_APP_HOME/cddear/fd.cdd" "$BE_APP_HOME/cddear/fd.ear"
 fi
