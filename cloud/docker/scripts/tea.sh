ec7446c195f3e5451355302a6ae490025dc646b6
Naresh Kumar Thota <nthota@tibco.com>
2025-12-16 16:37:44 +0530

    few fixes

--- cloud/docker/scripts/tea.sh
+++ cloud/docker/scripts/tea.sh
@@ -1,6 +1,6 @@
 #supported TEA versions for be version
-TEA_VERSION_MAP_MIN=( "6.3.0:2.4.1" "6.3.1:2.4.2" "6.3.2:2.4.2"  "6.4.0:2.4.2" )
-TEA_VERSION_MAP_MAX=( "6.3.0:2.4.1" "6.3.1:2.4.2" "6.3.2:2.4.2"  "6.4.0:2.4.2" )
+TEA_VERSION_MAP_MIN=( "6.3.0:2.4.1" "6.3.1:2.4.2" "6.3.2:2.4.2"  "6.4.0:2.4.3" )
+TEA_VERSION_MAP_MAX=( "6.3.0:2.4.1" "6.3.1:2.4.2" "6.3.2:2.4.2"  "6.4.0:2.4.3" )
 
 teaMinVersion=$(echo $( getFromArray "$ARG_BE_VERSION" "${TEA_VERSION_MAP_MIN[@]}" ) | sed -e "s/x/9/g" )
 teaMaxVersion=$(echo $( getFromArray "$ARG_BE_VERSION" "${TEA_VERSION_MAP_MAX[@]}" ) | sed -e "s/x/9/g" )
